5621a369883af48c19895352fd8d41a79f081da4
Hector Nava <47047085+NavaH1@users.noreply.github.com>
2019-03-16 15:47:43 -0700

    Create nades.py

---
+++ data/nades.py
@@ -0,0 +1,58 @@
+def Grenades():
+    grenades = [
+        {
+            'id': '1',
+            'title': 'Molotov',
+            'AMMO': '',
+            'Damage': ''
+        },
+        {
+            'id': '2',
+            'title': 'Frag Grenade',
+            'AMMO': '',
+            'Damage': ''
+        },
+        {
+            'id': '3',
+            'title': 'Concussion Grenade',
+            'AMMO': '',
+            'Damage': ''
+        },
+        {
+            'id': '4',
+            'title': 'Combat Axe',
+            'AMMO': '',
+            'Damage': ''
+        },
+        {
+            'id': '5',
+            'title': '9-Bang',
+            'AMMO': '',
+            'Damage': ''
+        },
+        {
+            'id': '6',
+            'title': 'Acid Bomb',
+            'AMMO': '',
+            'Damage': ''
+        },
+        {
+            'id': '7',
+            'title': 'Cluster Grenade',
+            'AMMO': '',
+            'Damage': ''
+        },
+        {
+            'id': '8'
+            'title': 'Cymbal Monkey',
+            'AMMO': '',
+            'Damage': ''
+        },
+        {
+            'id': '9',
+            'title': 'Mesh Mine',
+            'AMMO': '',
+            'Damage': ''
+        }
+        return grenades
+    ]
