39af1c402956d33ec5c67ec8f47d6a7a00102c95
ayushs0218 <48629890+ayushs0218@users.noreply.github.com>
2019-03-16 15:14:08 -0700

    Create snipers.py

---
+++ data/snipers.py
@@ -0,0 +1,29 @@
+def Snipers():
+    snipers = [
+
+        {
+            'id': '1',
+            'title': 'Paladin HB50',
+            'AMMO': '.50 cal',
+            'Damage': '140'
+        },
+        {
+            'id': '2',
+            'title': 'Koshka',
+            'AMMO': '.338 cal',
+            'Damage': '120'
+        },
+        {
+            'id': '3',
+            'title': 'Outlaw',
+            'AMMO': '.338 cal',
+            'Damage': '110'
+        },
+        {
+            'id': '4',
+            'title': 'SDM',
+            'AMMO': '.338 cal',
+            'Damage': '95'
+        },
+        return assaultrifles
+                    ]
