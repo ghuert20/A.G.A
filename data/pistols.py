bd5b7601ad8bef7c303543526867f89d2a417e66
ayushs0218 <48629890+ayushs0218@users.noreply.github.com>
2019-03-16 15:35:51 -0700

    Create pistols.py

---
+++ data/pistols.py
@@ -0,0 +1,23 @@
+def Pistols():
+    pistols = [
+
+        {
+            'id': '1',
+            'title': 'Strife',
+            'AMMO': '.45 Cal'
+            'Damage': '46'
+        },
+        {
+            'id': '2',
+            'title': 'Mozu',
+            'AMMO': '.45 Cal',
+            'Damage': '62'
+        },
+        {
+            'id': '3',
+            'title': 'RK7 Garrison',
+            'AMMO': '.45 Cal',
+            'Damage': '32'
+        },
+        return pistols
+                    ]
