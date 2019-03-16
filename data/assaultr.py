85a168dd4d2f75cd36593eb75428a65444ef0d8b
ayushs0218 <48629890+ayushs0218@users.noreply.github.com>
2019-03-16 14:48:44 -0700

    Update assaultr.py

---
+++ data/assaultr.py
@@ -0,0 +1,11 @@
+def AssaultRifles():
+    assaultrifles = [
+
+        {
+            'id': 'Number of Gun',
+            'title': 'ICR - 7',
+            'AMMO': '5.56mm',
+            'Damage': '37',
+        }
+        return assaultrifles
+                    ]
