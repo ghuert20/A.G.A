524180405afd9897329c7f1a6e010164d76555df
ayushs0218 <48629890+ayushs0218@users.noreply.github.com>
2019-03-16 15:26:05 -0700

    Create launcher.py

---
+++ data/launcher.py
@@ -0,0 +1,17 @@
+def Launcher():
+    launcher = [
+
+        {
+            'id': '1',
+            'title': 'Hellion Salvo',
+            'AMMO': '.40 HEAT',
+            'Damage': '--'
+        },
+        {
+            'id': '2',
+            'title': 'War Machine',
+            'AMMO': '.40 HEAT',
+            'Damage': '--'
+        },
+        return launcher
+                    ]
