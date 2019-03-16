972d2bca3266798cc8a6fe97a7b609b10db4200d
ayushs0218 <48629890+ayushs0218@users.noreply.github.com>
2019-03-16 13:50:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello!"
+    return "Call  of Duty Black OPS III Stats"
 
 if __name__ == "__main__":
     app.run(debug=True, port=5633)
