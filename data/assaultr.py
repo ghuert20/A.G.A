49632435d040be5fc353a03cb7d4d430f2edcaa4
ayushs0218 <48629890+ayushs0218@users.noreply.github.com>
2019-03-16 17:42:33 -0700

    Update assaultr.py

--- data/assaultr.py
+++ data/assaultr.py
@@ -6,36 +6,42 @@ def AssaultRifles():
             'title': 'ICR - 7',
             'AMMO': '5.56mm',
             'Damage': '37',
+            'Image':  '/static/images/ICR-7.png'
         }
         {
             'id': '2',
             'title': 'KN - 57',
             'AMMO': '7.62mm',
             'Damage': '40',
+            'Image':  '/static/images/KN-57.png'
         }
         {
             'id': '3',
             'title': 'Rampart 17',
             'AMMO': '7.62 mm',
             'Damage': '46',
+            'Image':  '/static/images/Rampart 17.jpg'
         }
         {
             'id': '4',
             'title': 'Vapr-XKG',
             'AMMO': '5.56mm',
             'Damage': '36',
+            'Image':  '/static/images/VAPR - XKG.jpg'
         }
         {
             'id': '5',
             'title': 'Maddox RFB',
             'AMMO': '5.56mm',
             'Damage': '32',
+            'Image':  '/static/images/ICR-7.png'
         }
         {
             'id': '6',
             'title': 'Grav',
             'AMMO': '5.56mm',
             'Damage': '37',
+            'Image':  '/static/images/maddox rfb.jpg'
         }
         return assaultrifles
                     ]
