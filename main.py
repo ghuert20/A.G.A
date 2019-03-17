7f8c3cb3608532f8a27e1b9521148843cb977d74
moses1252 <amviramontes34@yahoo.com>
2019-03-16 18:22:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
-from app import AssaultRifles, TacticalRifile, LightMachineGuns, Shotguns, Melee, Grenades, Pistols
+from app import AssaultRifles, TacticalRifile, LightMachineGuns, Shotguns, Melee, Grenades, Pistols, Launcher
 
 app = Flask(__name__)
 
+AssaultRifles = AssaultRifles()
+TacticalRifile = TacticalRifile()
+LightMachineGuns = LightMachineGuns()
+Shotguns = Shotguns()
+Melee = Melee()
+Grenades = Grenades()
+Pistols = Pistols()
+Launcher = Launcher()
+
 @app.route("/")
 def index():
-    return render_template('index.html', assultrifles = Assault Rifles, taticalrifle = RacticalRifle, lightmachinegun = LighrMachineGuns, shotguns = Shotguns, malee = Melee, grenades = Grenades, pistols = Pistols)
+    return render_template('index.html', assultrifles = AssaultRifles, taticalrifle = TacticalRifle, lightmachinegun = LightMachineGuns, shotguns = Shotguns, malee = Melee, grenades = Grenades, pistols = Pistols, launcher = Launcher)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5685)
