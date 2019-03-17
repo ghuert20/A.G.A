45278c4a1ec31bf30aea486bbf27a482a64d8c0b
moses1252 <amviramontes34@yahoo.com>
2019-03-16 18:06:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', assultrifles = Assault Rifles, taticalrifle = RacticalRifle, lightmachinegun = LighrMachineGuns, shotguns = Shotguns, malee = Melee, grenades = Grenades, pistols = Pistols)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5685)
